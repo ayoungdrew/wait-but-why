83d4496e5c2d3ca8d2b7ff90c1f17eaa2322ff63
tvlangley <chibia2@gmail.com>
2018-02-28 16:24:16 -0500

    Add password confirmation on password change
    Add password confirmation on change password
    close #32

--- app/routes/change-password.js
+++ app/routes/change-password.js
@@ -12,20 +12,25 @@ export default Route.extend({
 
   actions: {
     changePassword (passwords) {
-      this.get('auth').changePassword(passwords)
-      .then(() => this.get('auth').signOut())
-      .then(() => this.transitionTo('sign-in'))
-      .then(() => {
+      if (passwords.next === passwords.confirmNext) {
+        this.get('auth').changePassword(passwords)
+        .then(() => this.get('auth').signOut())
+        .then(() => this.transitionTo('sign-in'))
+        .then(() => {
+          this.get('flashMessages')
+          .success('Successfully changed your password!');
+        })
+        .then(() => {
+          this.get('flashMessages').warning('You have been signed out.');
+        })
+        .catch(() => {
+          this.get('flashMessages')
+          .danger('There was a problem. Please try again.');
+        });
+      } else {
         this.get('flashMessages')
-        .success('Successfully changed your password!');
-      })
-      .then(() => {
-        this.get('flashMessages').warning('You have been signed out.');
-      })
-      .catch(() => {
-        this.get('flashMessages')
-        .danger('There was a problem. Please try again.');
-      });
+        .danger('Your new passwords must match.');
+      }
     },
   },
 });
