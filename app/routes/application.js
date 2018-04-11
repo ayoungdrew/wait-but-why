5b8a5ec50e2d98e82aa288824d4981b722867a2f
Chris <06cpayne@gmail.com>
2018-04-11 16:34:56 -0400

    Adds dumb error handler
    Smart error handler was gobbling errors

--- app/routes/application.js
+++ app/routes/application.js
@@ -20,17 +20,19 @@ export default Route.extend({
     },
 
     error (reason) {
+      console.error(reason)
+
       const unauthorized = reason.errors && reason.errors.some((error) =>
         error.status === '401'
       )
 
       if (unauthorized) {
         this.get('flashMessages')
-        .danger('You must be authenticated to access this page.')
+          .danger('You must be authenticated to access this page.')
         this.transitionTo('/sign-in')
       } else {
         this.get('flashMessages')
-        .danger('There was a problem. Please try again.')
+          .danger('There was a problem. Please try again.')
       }
 
       return false
