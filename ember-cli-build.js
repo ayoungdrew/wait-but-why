6c086555cdc82e822ba49c4859a4901833393b95
Chris <06cpayne@gmail.com>
2018-04-11 14:02:57 -0400

    Standardizes config files and removes deprecation
    Some of our config files still had ;s in them.
    In addition, since the update we've been getting a deprecation error.
    Removed both.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,14 +1,20 @@
-/*jshint node:true*/
+/* eslint-env node */
 /* global require, module */
-var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const EmberApp = require('ember-cli/lib/broccoli/ember-app')
 
-module.exports = function(defaults) {
-  var app = new EmberApp(defaults, {
+module.exports = function (defaults) {
+  const app = new EmberApp(defaults, {
+    eslint: {
+      testGenerator: 'qunit',
+      group: true,
+      rulesDir: '.eslintrc.json',
+      extensions: ['js']
+    },
     'ember-bootstrap': {
       'bootstrapVersion': 3,
       'importBootstrapFont': true,
       'importBootstrapCSS': false
     }
-  });
-  return app.toTree();
-};
+  })
+  return app.toTree()
+}
