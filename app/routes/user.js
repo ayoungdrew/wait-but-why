c11e91d9d0337fa43be0362c1402cdb1b9a10825
ayoungdrew <andrewyoung504@gmail.com>
2018-04-30 20:42:41 -0400

    Merged with add-follows branch
    Merged with add-follows branch

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
 import Route from '@ember/routing/route'
-=======
 import { alias } from '@ember/object/computed'
-import Route from '@ember/routing/route'
 import Component from '@ember/component'
 import { inject as service } from '@ember/service'
->>>>>>> add-follows
 import RSVP from 'rsvp'
 
 export default Route.extend({
@@ -19,15 +15,6 @@ export default Route.extend({
     return RSVP.hash({
       // return FIRST key-value 'posts' to load user's event posts
       posts: this.get('store').findAll('event')
-<<<<<<< HEAD
-        .then(results => results.filter((x) => {
-          return x.get('user_id').toString() === params.user_id
-        }))
-        .then(events => events.sortBy('date').reverse()),
-      // return SECOND key-value 'fellowUser' to load user's 'email'/username
-      fellowUser: this.get('store').findRecord('user', params.user_id)
-        .then((data) => data.get('email'))
-=======
       .then(results => results.filter((x) => {
         return x.get('user_id').toString() === params.user_id
       }))
@@ -51,16 +38,11 @@ export default Route.extend({
           .then(results => results.find((x) => {
             return x.get('followed_id') === Number(params.user_id) && x.get('follower_id') === this.get('auth.credentials.id')
           }))
->>>>>>> add-follows
     })
   },
 
   actions: {
     destroyEvent (event) {
-<<<<<<< HEAD
-      // End of action chain; destroys event post & comments
-=======
->>>>>>> add-follows
       event.destroyRecord()
         .then(() => { this.toast.success('Killed that event!', '', { positionClass: 'toast-bottom-right' }) })
         .then(() => this.refresh())
@@ -85,10 +67,6 @@ export default Route.extend({
     updateComment (editedComment) {
       const comment = editedComment
       comment.save()
-<<<<<<< HEAD
-        .then(() => { this.toast.success('Update great success!', '', { positionClass: 'toast-bottom-right' }) })
-        .catch((error) => { this.toast.error('Something wrong...', error, { positionClass: 'toast-bottom-right' }) })
-=======
       .then(() => { this.toast.success('Update great success!', '', { positionClass: 'toast-bottom-right' }) })
       .catch((error) => { this.toast.error('Something wrong...', error, { positionClass: 'toast-bottom-right' }) })
     },
@@ -117,7 +95,6 @@ export default Route.extend({
       target.destroyRecord()
         .then(() => this.refresh())
         .then(() => { this.toast.success('Unfollowed...', '', { positionClass: 'toast-bottom-right' }) })
->>>>>>> add-follows
     }
   }
 })
