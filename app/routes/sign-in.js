d5117e17052f6e0207197be307bd3d94bb851ce6
ayoungdrew <andrewyoung504@gmail.com>
2018-05-02 00:06:15 -0400

    Changed call-to-action links
    "start posting" now goes to new user/<current_user> view

--- app/routes/sign-in.js
+++ app/routes/sign-in.js
@@ -14,7 +14,7 @@ export default Route.extend({
     signIn (credentials) {
       return this.get('auth').signIn(credentials)
         .then(() => this.transitionTo('application'))
-        .then(() => { this.toast.success('Signed in!', '', { positionClass: 'toast-bottom-right' })})
+        .then(() => { this.toast.success('Signed in!', '', { positionClass: 'toast-bottom-right' }) })
     .catch((error) => { this.toast.error('Problem signing in...', error, { positionClass: 'toast-bottom-right' }) })
     }
   }
